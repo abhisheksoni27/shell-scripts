9a90bc38ccd4dd3eebb22733667b4c2059f1182a
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-06-01 13:54:07 +0530

    because

--- commit-to-github.sh
+++ commit-to-github.sh
@@ -1,1 +1,19 @@
 #!/bin/sh
+cd $1
+# Stage changes
+git add .
+
+# Commit
+if ["$2" == ""]; then
+    echo "Please Enter a commit message."
+    exit
+else
+    git commit -m "$2"
+fi
+
+# Push changes
+if ["$3" == ""]; then
+    git push origin master
+else
+    git push origin $3
+fi
